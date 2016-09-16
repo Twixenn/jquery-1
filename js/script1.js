3c655908b9cfcf46133da2eb6a1729605f602572
dlufr06 <dlufr06@gmail.com>
2016-09-16 10:23:46 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -11,7 +11,10 @@ $(document).ready(function() {
 });
 
 
+//Vi kör en hel del click-funktioner och det finns olika funktioner för olika events, men det finns ett annat sätt
+//vi skall testa on.("event",funktion);
 
+//läs mer https://api.jquery.com/on/
 
 //det finns fler fade kommandon, fadein och fadeout, fadetoggle växlar mellan dessa se mer på
 
